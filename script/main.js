d068eacb02bc7f25501bcaa8de11c067813312e3
enbee81 <nils.binder@icloud.com>
2019-08-13 16:02:00 +0200

    Style Adjustments

--- script/main.js
+++ script/main.js
@@ -1,7 +1,7 @@
 const range = document.getElementById('font-size-control');
 const result = document.getElementById('percent');
-const liquid = document.getElementById('fill');
-const surface = document.getElementById('surface');
+const vwLiquid = document.getElementById('fill');
+const vwSurface = document.getElementById('surface');
 const pxLiquid = document.getElementById('px-fill');
 const pxSurface = document.getElementById('px-surface');
 const calc = document.getElementById('calc');
@@ -13,10 +13,10 @@ const resultFrame = document.getElementById('resultframe').contentWindow;
 
 range.addEventListener("input", function () {
   update();
-  liquid.setAttribute("y", 190 - range.value);
-  surface.setAttribute("cy", 190 - range.value);
-  pxLiquid.setAttribute("y", range.value * 1 + 65);
-  pxSurface.setAttribute("cy", range.value * 1 + 65);
+  vwLiquid.setAttribute("y", 195 - range.value * 1.7);
+  vwSurface.setAttribute("cy", 195 - range.value * 1.7);
+  pxLiquid.setAttribute("y", range.value * 1.7 + 65);
+  pxSurface.setAttribute("cy", range.value * 1.7 + 65);
   pxSurface.setAttribute("rx", range.value * 48 / 170 + 27);
 }, false);
 
@@ -31,21 +31,23 @@ inputResolution.addEventListener("input", function () {
 }, false);
 
 function update() {
-  var percent = (range.value * 10 / 17).toFixed(0);
+  var percent = range.value;
   var fluid = (fontsize * percent / 100).toFixed(0)
   var fixed = fontsize - fluid;
   var calcFluid = (fluid / resolution * 100).toFixed(2);
   var cssResult = "calc(" + fixed + "px + " + calcFluid + "vw)";
   result.innerHTML = percent + "%";
   calc.innerHTML = cssResult;
-  resultFrame.postMessage(cssResult, "https://9elements.github.io/font-size-lab");
+  var host = window.location.href;
+  console.log(host);
+  resultFrame.postMessage(cssResult, host);
 };
 
 window.onload = function () {
   update();
-  liquid.setAttribute("y", 190 - range.value);
-  surface.setAttribute("cy", 190 - range.value);
-  pxLiquid.setAttribute("y", range.value * 1 + 65);
-  pxSurface.setAttribute("cy", range.value * 1 + 65);
+  vwLiquid.setAttribute("y", 195 - range.value * 1.7);
+  vwSurface.setAttribute("cy", 195 - range.value * 1.7);
+  pxLiquid.setAttribute("y", range.value * 1.7 + 65);
+  pxSurface.setAttribute("cy", range.value * 1.7 + 65);
   pxSurface.setAttribute("rx", range.value * 48 / 170 + 27);
 };
